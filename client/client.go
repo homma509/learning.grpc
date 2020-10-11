8be41041900f8179a8efc944094e4643c980be1d
homma509 <kouki.honma@gmail.com>
2020-10-11 15:32:43 +0900

    request cancel

--- client/client.go
+++ client/client.go
@@ -7,10 +7,7 @@ import (
 	"time"
 
 	pb "github.com/homma509/learning.grpc"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 func main() {
@@ -24,24 +21,16 @@ func main() {
 
 	name := os.Args[1]
 
-	ctx := context.Background()
-	md := metadata.Pairs("timestamp", time.Now().Format(time.Stamp))
-	ctx = metadata.NewOutgoingContext(ctx, md)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		cancel()
+	}()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
-		s, ok := status.FromError(err)
-		if ok {
-			log.Printf("gRPC Error (message: %s)", s.Message())
-			for _, d := range s.Details() {
-				switch info := d.(type) {
-				case *errdetails.RetryInfo:
-					log.Printf(" RetryInfo: %v", info)
-				}
-			}
-			os.Exit(1)
-		} else {
-			log.Fatalf("could not greet: %v", err)
-		}
+		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.Message)
 }
